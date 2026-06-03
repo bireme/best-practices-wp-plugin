7010b57e960b931c8930476c74cee5ae4f3a4807
gabrielszz <50212683+gabrielszz@users.noreply.github.com>
2026-06-03 14:50:31 -0300

    Ajuste de limite para ordenação de todo conteúdo.

--- widgets.php
+++ widgets.php
@@ -36,8 +36,8 @@ class Best_Practices_Widget extends WP_Widget {
 
         $bp_config = get_option('bp_config');
         //$bp_service_request = $this->service_url . '/api/bp?limit=' . $instance['total'] . '&lang=' . $locale[$lang];
-        $bp_service_request = $this->service_url . '/api/bp?&lang=' . $locale[$lang];
-		echo $bp_service_request;
+        $bp_service_request = $this->service_url . '/api/bp?limit=120&lang=' . $locale[$lang];
+		//echo $bp_service_request;
 
 		
 		//echo $bp_service_request;
